c0fdd28f9e497efca8bdf01f24227e7d33e7b98c
OldMan35 <kostya_kv@mail.ru>
2021-03-12 19:19:14 +0300

    добавление файлов проекта

---
+++ src/main/java/com/example/CurrencyConverter/Main.java
@@ -0,0 +1,13 @@
+package com.example.CurrencyConverter;
+
+import com.example.CurrencyConverter.HttpResponse.URLInput;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        URLInput start = new URLInput();
+        start.getValue();
+
+    }
+}
