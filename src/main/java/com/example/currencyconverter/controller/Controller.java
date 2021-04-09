efe589c3f36f1de8cda892e0911aa94c9a5388e7
OldMan35 <kostya_kv@mail.ru>
2021-04-09 22:55:14 +0300

    added classes. fix bag jparser&jobject

--- src/main/java/com/example/currencyconverter/controller/Controller.java
+++ src/main/java/com/example/currencyconverter/controller/Controller.java
@@ -1,4 +1,68 @@
 package com.example.currencyconverter.controller;
 
-public class Controller {
+import com.example.currencyconverter.parserjson.ParserJson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Controller extends ParserJson {
+    private String firstValue;
+    private String secondValue;
+    private double value;
+    private String valuteName;
+    private long valuteNominal;
+    private double secondValueValute;
+    private String secondNameValute;
+    private long secondNominalValute;
+
+    public void conversionCurrency() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите валюту для конвертации: ");
+        firstValue = reader.readLine(); // ввод данных. получаем данные (вид валюты) от пользователя
+        setCurrency(firstValue);//устанавливаем вид валюты для конвертации
+        parser();
+        value = (double) getValuteValue();//значение валюты
+        valuteName = (String) getValuteName();//название валюты
+        valuteNominal = (long) getValuteNominal();//номинал валюты
+        System.out.println("Номинал: " + valuteNominal + " " + valuteName);
+        System.out.println(value + " " + " rub.");
+
+        secondValue = reader.readLine(); // получаем данные (вид валюты) от пользователя
+        setCurrency(secondValue);//устанавливаем вид валюты для конвертации
+        parser();
+        secondValueValute = (double) getValuteValue();
+        secondNameValute = (String) getValuteName();
+        secondNominalValute = (long) getValuteNominal();
+        System.out.println("Номинал: " + secondNominalValute + " " + secondNameValute);
+        System.out.println(secondValueValute + " " + " rub.");
+
+        System.out.println("конец программы");
+
+    }
+
+    public String getFirsValue() {
+
+        return firstValue;
+    }
+
+    public void setFirsValue(String firsValue) {
+
+        this.firstValue = firsValue;
+    }
+
+    public String getSecondValue() {
+
+        return secondValue;
+    }
+
+    public void setSecondValue(String secondValue) {
+
+        this.secondValue = secondValue;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.conversionCurrency();
+    }
 }
+
--- src/main/java/com/example/currencyconverter/parserjson/ParserJson.java
+++ src/main/java/com/example/currencyconverter/parserjson/ParserJson.java
@@ -1,4 +1,56 @@
 package com.example.currencyconverter.parserjson;
 
+
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStream;
+import java.net.URL;
+
 public class ParserJson {
+    private String currency;
+    private final String name = "Name";
+    private final String value = "Value";
+    private final String valute = "Valute";
+    private final String nominal = "Nominal";
+    private Object valuteNominal;
+    private Object valuteName;
+    private Object valuteValue;
+
+    public void parser() throws Exception {
+
+        URL url = new URL("https://www.cbr-xml-daily.ru/daily_json.js");//Создает объект URL с путем к странице
+        InputStream input = url.openStream();//Получает InputStream у интернет-объекта
+        byte[] buffer = input.readAllBytes();//Читает все байты и возвращает массив байт
+        String strings = new String(buffer);//Преобразуем массив в строку
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(strings);// парсим строку
+        JSONObject jsonObject = (JSONObject) obj;//получаем объект JSON
+        JSONObject jsonСontent = (JSONObject) jsonObject.get(valute);//разбираем объект по значениям
+        JSONObject valuteСontent = (JSONObject) jsonСontent.get(currency);//получаем запрос на вид валюты от пользователя
+        valuteNominal = valuteСontent.get(nominal);//получаем номинал валюты
+        valuteName = valuteСontent.get(name);//название валюты для вывода пользователю
+        valuteValue = valuteСontent.get(value);//значение валюты для расчета значения конвертации
+
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Object getValuteName() {
+
+        return valuteName;
+    }
+
+    public Object getValuteValue() {
+
+        return valuteValue;
+    }
+
+    public Object getValuteNominal() {
+
+        return valuteNominal;
+    }
 }
