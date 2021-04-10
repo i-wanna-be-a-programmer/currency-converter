20d9231941801becdbc6133ae634c66108f22020
OldMan35 <kostya_kv@mail.ru>
2021-04-10 09:48:08 +0300

    current changes

--- src/main/java/com/example/currencyconverter/controller/Controller.java
+++ src/main/java/com/example/currencyconverter/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.example.currencyconverter.controller;
 
+import com.example.currencyconverter.currencyexchangerate.CurrencyExchangeRate;
 import com.example.currencyconverter.parserjson.ParserJson;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 // оставить этот класс только для запуска приложения, логику убрать?
 public class Controller extends ParserJson {
     private String firstValue;
@@ -36,28 +38,77 @@ public class Controller extends ParserJson {
         secondNominalValuteController = (long) getValuteNominal();
         System.out.println("Nominal : " + secondNominalValuteController + " " + secondNameValuteController);
         System.out.println(secondValueValuteController + " " + " rub.");
-
+        double result = (int) (getValueController() / getSecondValueValuteController());
+        System.out.println(result);
         System.out.println("end");
 
     }
 
-    public String getFirsValue() {
+    public String getFirstValue() {
+        return firstValue;
+    }
 
+    public void setFirstValue(String firstValue) {
+        this.firstValue = firstValue;
+    }
+
+    public double getValueController() {
+        return valueController;
+    }
+
+    public void setValueController(double valueController) {
+        this.valueController = valueController;
+    }
+
+    public String getValuteNameController() {
+        return valuteNameController;
+    }
+
+    public void setValuteNameController(String valuteNameController) {
+        this.valuteNameController = valuteNameController;
+    }
+
+    public void setValuteNominalController(long valuteNominalController) {
+        this.valuteNominalController = valuteNominalController;
+    }
+
+    public double getSecondValueValuteController() {
+        return secondValueValuteController;
+    }
+
+    public void setSecondValueValuteController(double secondValueValuteController) {
+        this.secondValueValuteController = secondValueValuteController;
+    }
+
+    public String getSecondNameValuteController() {
+        return secondNameValuteController;
+    }
+
+    public void setSecondNameValuteController(String secondNameValuteController) {
+        this.secondNameValuteController = secondNameValuteController;
+    }
+
+    public long getSecondNominalValuteController() {
+        return secondNominalValuteController;
+    }
+
+    public void setSecondNominalValuteController(long secondNominalValuteController) {
+        this.secondNominalValuteController = secondNominalValuteController;
+    }
+
+    public String getFirsValue() {
         return firstValue;
     }
 
     public void setFirsValue(String firsValue) {
-
         this.firstValue = firsValue;
     }
 
     public String getSecondValue() {
-
         return secondValue;
     }
 
     public void setSecondValue(String secondValue) {
-
         this.secondValue = secondValue;
     }
 
@@ -70,10 +121,10 @@ public class Controller extends ParserJson {
     }
 }
 
-class ControllerTest {
-    public static void main(String[] args) throws Exception {
-        Controller controller = new Controller();
-        controller.conversionCurrency();
-    }
-}
+//class ControllerTest {
+//    public static void main(String[] args) throws Exception {
+//        Controller controller = new Controller();
+//        controller.conversionCurrency();
+//    }
+//}
 
--- src/main/java/com/example/currencyconverter/currencyexchangerate/CurrencyExchangeRate.java
+++ src/main/java/com/example/currencyconverter/currencyexchangerate/CurrencyExchangeRate.java
@@ -5,11 +5,23 @@ import com.example.currencyconverter.controller.Controller;
 public class CurrencyExchangeRate extends Controller {
 
 
-    public void rate() {
-        if (getValuteNominalController() != 1) {
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.conversionCurrency();
 
-        }
 
+//        currencyExchangeRate.rate();
 
     }
+
+//    public void rate() {
+//        if (getValuteNominalController() == 1) {
+//            double result = getValueController() * 1.0 / getSecondValueValuteController();
+//            System.out.println(result);
+//        } else {
+//            System.out.println("Something's wrong!");
+//        }
+//
+//
+//    }
 }
